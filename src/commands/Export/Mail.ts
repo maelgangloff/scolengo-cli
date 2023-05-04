08be93a5cad8cd0596f0f607c28b06ca487581eb
Maël Gangloff <contact@maelgangloff.fr>
2023-05-04 21:10:03 +0200

    feat(export/mail): :sparkles: add communication id in the file name

--- src/commands/Export/Mail.ts
+++ src/commands/Export/Mail.ts
@@ -42,6 +42,7 @@ async function mail (filePath: string, {
       const zip = new JSZip()
       for (const communication of communications) {
         const subject = communication.communication.subject
+        const communicationId = communication.communication.id
         const participations = communication.participations
 
         for (let i = 0; i < participations.length; i++) {
@@ -51,7 +52,7 @@ async function mail (filePath: string, {
             id
           } = participations[i]
           const senderName = ((sender?.person) != null) ? `${sender.person.firstName} ${sender.person.lastName}` : sender?.technicalUser?.label
-          const fileName = `${id}-${escapeFileName(senderName ?? 'Inconnu')}-${escapeFileName(subject)}.eml`
+          const fileName = `${communicationId}-${id}-${escapeFileName(senderName ?? 'Inconnu')}-${escapeFileName(subject)}.eml`
           const contentMIME = participationToMIME(participations[i], subject, i !== 0 ? participations[0].id : undefined)
           const content = attachments.length > 0 ? contentMIME + `<br>__________<br>Pièces jointes :<br>${attachments.map(a => `  - ${a.name ?? 'Inconnu'} (${a.mimeTypeLabel ?? 'Inconnu'}) : <a href="${a.url}">${a.url}</a>`).join('<br>')}` : contentMIME
           zip.file(fileName, content)
