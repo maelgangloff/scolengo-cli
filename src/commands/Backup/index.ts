80c3cf0b01e28e2c1dfc7b210f49a962c36950a7
Maël Gangloff <contact@maelgangloff.fr>
2023-05-08 22:24:31 +0200

    refactor(backup): :construction: optimize ok status message

--- src/commands/Backup/index.ts
+++ src/commands/Backup/index.ts
@@ -6,13 +6,14 @@ import chalk from 'chalk'
 import { getCredentials } from '../../store'
 import { Skolengo } from 'scolengo-api'
 import {
-  attachmentsToZip, communicationsToZip,
+  attachmentsToZip,
+  communicationsToZip,
   getAbsencesFiles,
   getCommunications,
+  getDateFromISO,
   getEvaluation,
   onTokenRefreshVerbose,
-  periodsToCSV,
-  getDateFromISO
+  periodsToCSV
 } from '../../functions'
 
 enum ExportableData {
@@ -46,6 +47,10 @@ async function backup (filePath: string, {
       type: 'checkbox',
       name: 'exportList',
       message: 'Choix des données à exporter',
+      validate (input: ExportableData[], answers?: any): boolean {
+        if (input.length > 0) return true
+        throw new Error('Veuillez effectuer un choix non vide.')
+      },
       choices: [
         {
           name: 'Absences',
@@ -62,14 +67,14 @@ async function backup (filePath: string, {
           disabled: permissions !== undefined && !permissions.includes('READ_LESSONS')
         },
         {
-          name: 'Courriels de la boîte de réception',
-          value: ExportableData.MAIL,
-          disabled: permissions !== undefined && !permissions.includes('READ_MESSAGES')
-        },
-        {
           name: 'Relevé de notes',
           value: ExportableData.NOTES,
           disabled: permissions !== undefined && !permissions.includes('READ_EVALUATIONS')
+        },
+        {
+          name: 'Courriels de la messagerie interne',
+          value: ExportableData.MAIL,
+          disabled: permissions !== undefined && !permissions.includes('READ_MESSAGES')
         }
       ].map(c => ({
         ...c,
@@ -86,35 +91,30 @@ async function backup (filePath: string, {
       const folder = zip.folder('absences') as JSZip
       folder.file('absences.csv', absences.toCSV())
       folder.file('absences.json', JSON.stringify(absences, null, 2))
-      console.log(chalk.green('✔ Absences'))
     } else if (exportableData === ExportableData.BULLETINS) {
       const folder = zip.folder('bulletins') as JSZip
       const bulletins = await user.getPeriodicReportsFiles(studentId, 100)
       await attachmentsToZip(user, folder, bulletins)
-      console.log(chalk.green('✔ Bulletins'))
     } else if (exportableData === ExportableData.CALENDAR) {
       const folder = zip.folder('calendar') as JSZip
       const agenda = await user.getAgenda(studentId, getDateFromISO(new Date()), getDateFromISO(new Date(Date.now() + 90 * 24 * 60 * 60 * 1e3)))
       folder.file('calendar.ics', agenda.toICalendar())
       folder.file('calendar.json', JSON.stringify(agenda, null, 2))
-      console.log(chalk.green('✔ Calendar'))
     } else if (exportableData === ExportableData.NOTES) {
       const folder = zip.folder('notes') as JSZip
       const notes = await getEvaluation(user, studentId)
       folder.file('notes.csv', periodsToCSV(notes))
       folder.file('notes.json', JSON.stringify(notes, null, 2))
-      console.log(chalk.green('✔ Notes'))
     } else if (exportableData === ExportableData.MAIL) {
-      const folder = zip.folder('mail') as JSZip
       const mailSettings = await user.getUsersMailSettings(credentials.userId)
-      const inboxFolder = mailSettings.folders.find(f => f.type === 'INBOX')
-      if (inboxFolder === undefined) throw new Error('Impossible de trouver la boîte de réception du courriel')
-
-      const communications = await getCommunications(user, inboxFolder)
-      await communicationsToZip(user, folder.folder('eml') as JSZip, communications, attachments)
-      folder.file('communications.json', JSON.stringify(communications, null, 2))
-      console.log(chalk.green('✔ Mail'))
+      for (const mailFolder of mailSettings.folders) {
+        const folder = (zip.folder('mail') as JSZip).folder(mailFolder.type) as JSZip
+        const communications = await getCommunications(user, mailFolder)
+        await communicationsToZip(user, folder.folder('eml') as JSZip, communications, attachments)
+        folder.file('communications.json', JSON.stringify(communications, null, 2))
+      }
     }
+    console.log(chalk.green('✔ ' + ExportableData[exportableData]))
   }
 
   zip.generateNodeStream({
