44a8568a98f32b0e585dcfd5d9e106d961ead451
Maël Gangloff <contact@maelgangloff.fr>
2023-05-09 21:17:58 +0200

    feat(export/mail): :sparkles: add progressbar when downloading participations

--- src/functions/getCommunications.ts
+++ src/functions/getCommunications.ts
@@ -1,5 +1,7 @@
 import { Communication, Folder, Participation } from 'scolengo-api/types/models/Messaging'
 import { Skolengo } from 'scolengo-api'
+import cliProgress from 'cli-progress'
+import chalk from 'chalk'
 
 export async function getCommunications (user: Skolengo, folder: Folder, limit?: string): Promise<Array<{ communication: Communication, participations: Participation[] }>> {
   let offset = 0
@@ -12,10 +14,18 @@ export async function getCommunications (user: Skolengo, folder: Folder, limit?:
     offset += n
     boiteReception.push(...newCommunications)
   }
+  const uniqueCommunications = boiteReception.filter((value: Communication, index: number, self: Communication[]) => index === self.findIndex((t: Communication) => t.id === value.id))
 
-  return await Promise.all(boiteReception.filter((value: Communication, index: number, self: Communication[]) =>
-    index === self.findIndex((t: Communication) => t.id === value.id)).map(async (c: Communication) => ({
-    communication: c,
-    participations: await user.getCommunicationParticipations(c.id)
-  })))
+  const statusBar = new cliProgress.SingleBar({ clearOnComplete: true, format: `[${chalk.greenBright('{bar}')}] {percentage}% | ETA: {eta}s | {value}/{total} {title}` }, cliProgress.Presets.rect)
+  statusBar.start(uniqueCommunications.length, 0)
+
+  const result = await Promise.all(uniqueCommunications.map(async (c: Communication) => {
+    statusBar.increment({ title: c.subject })
+    return {
+      communication: c,
+      participations: await user.getCommunicationParticipations(c.id)
+    }
+  }))
+  statusBar.stop()
+  return result
 }
